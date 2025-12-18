e3e54f208359b1f6f79b17f9241f3698eab19b9c
bala-amypo <bala@amypo.in>
2025-12-18 04:39:19 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9334.pro604cr.amypo.ai/")
+                        new Server().url("https://9424.pro604cr.amypo.ai/")
                 ));
         }
 }
